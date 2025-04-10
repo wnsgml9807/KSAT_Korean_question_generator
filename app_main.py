8148665db8ea39e82b05a0f92139c480e18298bc
wnsgml9807 <wnsgml9807@naver.com>
2025-04-11 01:37:12 +0900

    update

--- app_main.py
+++ app_main.py
@@ -1,10 +1,11 @@
+import streamlit as st
 # ChromaDB가 최신 sqlite3를 사용하도록 설정
-if st.runtime.exists(): 
+if st.secrets:
     __import__('pysqlite3')
     import sys
     sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 
-import streamlit as st
+
 import requests
 import time
 import os
